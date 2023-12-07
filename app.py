1644b15ece1a899866b4f8f51a637e353b95c531
Andik1402 <andika140202@gmai.com>
2023-12-07 17:38:04 +0700

    update login

--- app.py
+++ app.py
@@ -1,9 +1,68 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from datetime import datetime, timedelta
+
+
 app = Flask(__name__)
 
+SECRET_KEY = "SNAPDARGON"
+
+client = MongoClient('mongodb+srv://Andika02:Andika0202@cluster0.9ohxqnd.mongodb.net/?retryWrites=true&w=majority')
+
+db = client['snapdragon']
+
 @app.route('/')
+def home():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('account.html', user_info=user_info, active_page='account')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="Your token has expired"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="There was problem logging you in"))
+
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route("/sign_in", methods=["POST"])
+def sign_in():
+    username_receive = request.form["username_give"]
+    password_receive = request.form["password_give"]
+    pw_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+    result = db.users.find_one(
+        {
+            "username": username_receive,
+            "password": pw_hash,
+        }
+    )
+    if result:
+        payload = {
+            "id": username_receive,
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+        return jsonify(
+            {
+                "result": "success",
+                "token": token,
+            }
+        )
+    else:
+        return jsonify(
+            {
+                "result": "fail",
+                "msg": "We could not find a user with that username/password combination",
+            }
+        )
+
 
 @app.route('/dashboard')
 def dashboard():
