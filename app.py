d6f04bb3eb174cf2df0864ea02640afa6e95b175
Andik1402 <andika140202@gmai.com>
2023-12-07 13:07:35 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', active_page='dashboard')
+
+@app.route('/Acoount')
+def account():
+    return render_template('account.html', active_page='account')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
