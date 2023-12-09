b741e82646572b3e7c57a4ac699e4d9e649b43d4
Andik1402 <andika140202@gmai.com>
2023-12-09 17:58:49 +0700

    update dosen add, edit, delete

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
+from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["UPLOAD_FOLDER"] = "./static/myassets/profile"
 
 SECRET_KEY = "SNAPDARGON"
 
@@ -75,10 +79,95 @@ def dashboard():
         return render_template('dashboard.html', active_page='dashboard', role=role)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+    
+@app.route('/manajemen-dosen')
+def mnjmdosen():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        if user_info and user_info.get('role') in ['superuser']:
+            semua_dosen = db.users.find({'role': 'dosen'})
+            return render_template("admin/mnjmdosen.html", dosen=semua_dosen, active_page="mnjm_dosen", user_info=user_info)
+        else:
+            return redirect(url_for("home"))
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+    
+@app.route('//manajemen-dosen/checknip', methods=['POST'])
+def check_nip():
+    nip_receive = request.form['nip_give']
+    exists = bool(db.users.find_one({"username": nip_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+@app.route('/manajemen-dosen/savedosen', methods=['POST'])
+def savedosen():
+    nip_receive = request.form['nip']
+    password_receive = 'Dosen@123'
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    fname_receive = request.form['fname']
+    tl_receive = request.form['tl']
+    gender_receive = request.form['gender']
+
+    doc = {
+        "username": nip_receive,
+        "password": password_hash,
+        "role": "dosen",
+        "full_name": fname_receive,
+        "tanggal_lahir": tl_receive,
+        "gender": gender_receive,
+        "profile_pic": "",                                         
+        "profile_pic_real": "profile/2.jpg",
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/editdosen/<string:id>', methods=['GET', 'POST'])
+def editdosen(id):
+    token_receive = request.cookies.get("mytoken")
+    data = db.users.find_one({'_id': ObjectId(id)}, {'username':payload.get('id')})
+    if request.method == 'POST':
+        fname_receive = request.form['edit-nama-dosen']
+        tl_receive = request.form['edit-brithday-dosen']
+        gender_receive = request.form['edit-gender-dosen']
+
+        db.users.update_one({'_id': ObjectId(id)}, {'$set': {'full_name': fname_receive, 'tanggal_lahir': tl_receive, 'gender': gender_receive}})
+        return redirect(url_for('mnjmdosen'))
+    
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({'username':payload.get('id')})
+        selected_gender = data.get('gender', 'male')
+        return render_template('admin/editdosen.html', data=data, active_page="mnjm_dosen",selected_gender=selected_gender, user_info=user_info)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/deletedosen/<string:id>')
+def delete(id):
+    db.users.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('mnjmdosen'))
+
+def get_user_role():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        role = user_info.get('role')
+        return role
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/manajemen-mahasiswa')
+def mnjm_mhs():
+    return 'mahasiswa'
 
 @app.route('/Acoount')
 def account():
     return render_template('account.html', active_page='account')
+
+@app.context_processor
+def inject_role():
+    return {'user_role': get_user_role()}
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
